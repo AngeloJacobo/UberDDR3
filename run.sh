f4f0a5c11c4b1d9b73d102781f6bbb8939e39954
Angelo Jacobo <jacobo.angeloECE@gmail.com>
2023-05-28 16:24:22 +0800

    Update run.sh with the new ddr3 files

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 if [ "$1" == "" ]
 then
     yosys -q -p "
+    read_verilog -sv ./rtl/ddr3_top.v;
     read_verilog -sv ./rtl/ddr3_controller.v;
-    synth -top ddr3_controller"
+    read_verilog -sv ./rtl/ddr3_phy.v;
+    synth -top ddr3_top"
 
 elif [ "$1" == "iverilog" ] 
 then
-    iverilog ./rtl/ddr3_controller.v -I ./rtl/ -o .out
+    iverilog ./rtl/ddr3_top.v ./rtl/ddr3_controller.v ./rtl/ddr3_phy.v -o .out
     vvp .out
-
 fi
 
 # :set fileformat=unix
