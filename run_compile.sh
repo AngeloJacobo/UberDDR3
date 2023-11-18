6d594a0dbb8d66abd8214e1ee1c0a673a01c2761
AngeloJacobo <jacobo.angeloECE@gmail.com>
2023-11-18 13:36:29 +0800

    use multiple-task sby with summary at end

--- run_compile.sh
+++ run_compile.sh
@@ -24,5 +24,23 @@ echo
 echo ""
 echo -e "\e[32mRun Symbiyosys Formal Verification:\e[0m"
 echo "---------------------------------------"
-sby -f ddr3_controller.sby
+sby -f ddr3.sby
+
+
+# ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Iterate over folders starting with 'ddr3*'
+for folder in ddr3*/ ; do
+    # Check if the 'PASS' file exists in the folder
+    if [[ -e "${folder}PASS" ]]; then
+        # Print the folder name and 'PASS' in green
+        echo -e "${folder}: ${GREEN}PASS${NC}"
+    else
+        # Print the folder name and 'FAIL' in red
+        echo -e "${folder}: ${RED}FAIL${NC}"
+    fi
+done
 
