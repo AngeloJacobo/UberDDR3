15fe5af5d92be845c62830fa1b98c3b873b385bc
AngeloJacobo <jacobo.angeloECE@gmail.com>
2023-11-26 13:35:59 +0800

    add extra line

--- run_compile.sh
+++ run_compile.sh
@@ -32,6 +32,9 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+echo ""
+echo ""
+echo "Summary:"
 # Iterate over folders starting with 'ddr3*'
 for folder in ddr3*/ ; do
     # Check if the 'PASS' file exists in the folder
